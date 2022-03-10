2aac47a714f016c8375bf7407a5de294761d9991
DonSimonetti <matt.simonetti@hotmail.com>
2022-03-10 14:52:58 +0100

    Added prototype of a cached factorial function

---
+++ src/myFactorial.py
@@ -0,0 +1,21 @@
+cached_values = [1]
+
+
+def generate_factorials(size):
+    cached_values.clear()
+    cached_values.append(1)
+    for i in range(size):
+        cached_values.append(cached_values[i] * (i + 1))
+
+
+def factorial(value):
+    if len(cached_values) - 1 < value:
+        generate_factorials(value)
+    return cached_values[value]
+
+# Test
+generate_factorials(11)
+
+print(cached_values)
+print("min calculated value:", len(cached_values) - 1)
+print(factorial(40))
