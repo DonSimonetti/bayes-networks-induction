71a7d4feb06db630fde5eec913363720ab7f916a
matteodev <matt.simonetti@hotmail.com>
2022-03-12 20:32:00 +0100

    Managed to get a list from the pandas DataFrame

--- src/dataset_generator.py
+++ src/dataset_generator.py
@@ -1,5 +1,10 @@
-import torch
 import bnlearn
 
 bn = bnlearn.import_DAG('../alarm.bif')
-samples = bnlearn.sampling(bn, 1000)
+samples = bnlearn.sampling(bn, 100)
+
+samples_list = samples.values.tolist()
+print(samples.columns)
+for i in samples.columns:
+    print(i)
+print(len(samples.columns))
