ade82952aef8e2dd7c7c0db554df0878429c525f
matteodev <matt.simonetti@hotmail.com>
2022-03-14 16:02:15 +0100

    Implementing and testing get_data_in_column

---
+++ src/tests/get_data_in_column_test.py
@@ -0,0 +1,35 @@
+import pickle
+
+
+def get_data_in_column(dataset, columns):
+    print("Selecting",columns)
+    result_list = []
+
+    columns_indexes = []
+    for column in columns:
+        columns_indexes.append(dataset[0].index(column))
+
+    # print(columns_indexes)
+
+    for i in range(1, len(dataset)):
+        vec = []
+        for index in columns_indexes:
+            vec.append(dataset[i][index])
+        result_list.append(vec)
+
+    return result_list
+
+
+_dataset = open("10_samples_dataset.obj", "rb")
+serialized_str = _dataset.read()
+_dataset.close()
+deserialized_set = pickle.loads(serialized_str)
+for i in deserialized_set:
+    print(i)
+
+_columns = get_data_in_column(deserialized_set, ['HISTORY', 'CVP', 'PCWP', 'BP'])
+
+
+#print(_columns)
+#for j in _columns:
+    #print(j)
