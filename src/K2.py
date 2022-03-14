2187e91b724efc4fb6eea666b7f17ed31a2852f4
matteodev <matt.simonetti@hotmail.com>
2022-03-14 16:27:30 +0100

    Further work done in implementing K2

--- src/K2.py
+++ src/K2.py
@@ -1,10 +1,48 @@
 from node import Node
 
 
-def g_function(node, parents, case_set):  # TODO
-    prob = 0.0
+def get_distinct_data_in_columns(dataset, columns):
+    print("Selecting",columns)
+    result_list = []
 
-    return prob
+    columns_indexes = []
+    for column in columns:
+        columns_indexes.append(dataset[0].index(column))
+
+    # print(columns_indexes)
+
+    for i in range(1, len(dataset)):
+        vec = []
+        for index in columns_indexes:
+            vec.append(dataset[i][index])
+        if vec not in result_list:
+            result_list.append(vec)
+
+    return result_list
+
+
+def g_function(node, parents, case_set):  # TODO
+
+    # TODO get the number of possible values for node (i.e. r_i)
+
+    parents_instances = []  # I.E. the w_i, all the instances of 'parents' in the dataset
+    parents_instances_size = 0  # I.E. the q_i
+    if len(parents) == 0:
+        # return algebraic multiplicity of its distribution image
+        return
+    else:
+        parents_instances = get_distinct_data_in_columns(case_set, parents)  # the w_i
+        parents_instances_size = len(parents_instances)  # the q_i
+
+    # here all the calculation
+    result = 0.0
+    for j in range(parents_instances_size):  # the q_i loop
+        # TODO calculate result = (r_i - 1)!/(Nij + r_i - 1)!
+        # for k in range(r_i):
+        # TODO calculate result *= Nijk
+        print(j)
+
+    return result
 
 
 def K2(nodes_array, order_array, max_parents, cases_database):  # TODO
