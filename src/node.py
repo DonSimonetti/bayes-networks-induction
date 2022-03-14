ba77068ed89137197f9631d62fe19aabbb69cca1
matteodev <matt.simonetti@hotmail.com>
2022-03-14 16:26:31 +0100

    Added new attributes

--- src/node.py
+++ src/node.py
@@ -1,6 +1,8 @@
 class Node:
 
     def __init__(self):
+        self.name = ''
         self.parents = []
-        self.var_domain_size = 0
+        self.var_domain = []
+        self.var_domain_size = 0  # I.E. the r_i possible value assignments
         return
