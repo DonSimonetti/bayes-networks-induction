f8a32b1a93245799f00579fa421e45883b875196
matteodev <matt.simonetti@hotmail.com>
2022-03-17 12:15:53 +0100

    More refactoring (removing absolute values)

--- src/tests/dataframe_tests.py
+++ src/tests/dataframe_tests.py
@@ -1,4 +1,5 @@
 import bnlearn
+from src import node
 
 bn = bnlearn.import_DAG('../../alarm.bif')
 samples = bnlearn.sampling(bn, 10)
@@ -50,12 +51,11 @@ print("putting all together..")
 
 parents_i_names = ['HISTORY', 'HYPOVOLEMIA']
 parents_i_distinct_occurrences = []
-parents_i_distinct_occurrences_count = []
 
 node_i_name = 'CO'
 node_i_possible_values = bn['model'].states[node_i_name]
 
-filter_node = samples['CO'] == 0
+filter_node = samples[node_i_name] == 0
 
 print("v_i =", node_i_possible_values, "=> r_i =", len(node_i_possible_values))
 print("pi_i =", parents_i_names)
@@ -63,9 +63,6 @@ print("pi_i =", parents_i_names)
 for i in samples.filter(items=parents_i_names).values.tolist():
     if i not in parents_i_distinct_occurrences:
         parents_i_distinct_occurrences.append(i)
-        parents_i_distinct_occurrences_count.append(1)
-    else:
-        parents_i_distinct_occurrences_count[parents_i_distinct_occurrences.index(i)] += 1
 
 print("W_i =", parents_i_distinct_occurrences, "=> q_i =", len(parents_i_distinct_occurrences))
 
@@ -77,9 +74,9 @@ for j in parents_i_distinct_occurrences:
     print("for j =", j)
     N_ij = 0
     for k in node_i_possible_values:
-        print("\tcalculating N_ij set where 'CO' =", k, "( more precisely N_ijk where k =", k, ")")
+        print("\tcalculating N_ij set where '"+node_i_name+"' =", k, "( more precisely N_ijk where k =", k, ")")
         pi_i_instances = samples.filter(items=parents_i_names) \
-            .where(samples['CO'] == k).copy().dropna().values.tolist()
+            .where(samples[node_i_name] == k).copy().dropna().values.tolist()
 
         # re-cast to int
         for i in pi_i_instances:
