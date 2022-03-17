26b735a4861a325279081764e0285363bc1e510b
matteodev <matt.simonetti@hotmail.com>
2022-03-17 17:34:46 +0100

    Polishing

--- src/tests/g_function_test.py
+++ src/tests/g_function_test.py
@@ -8,9 +8,8 @@ samples = bnlearn.sampling(bn, 10)
 node_i = Node()
 node_i.var_name = 'CO'
 node_i.var_domain = bn['model'].states[node_i.var_name]
-node_i.var_domain_size = len(node_i.var_domain)  # maybe this is useless. I'll remove it when I'll be sure
 parents = ['HISTORY', 'HYPOVOLEMIA']
 
 result = g_function(node_i, parents, samples)
 
-print(result)
+print("g_function result:",result)
