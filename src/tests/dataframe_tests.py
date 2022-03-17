b7b63fdeea87b2e5ba82a8caa82ca5c32711d3fe
matteodev <matt.simonetti@hotmail.com>
2022-03-17 13:58:47 +0100

    Replaced vars with Nodes of node.py

--- src/tests/dataframe_tests.py
+++ src/tests/dataframe_tests.py
@@ -49,34 +49,35 @@ samples = bnlearn.sampling(bn, 10)
 
 print("putting all together..")
 
-parents_i_names = ['HISTORY', 'HYPOVOLEMIA']
+node_i = node.Node()
+node_i.var_name = 'CO'
+node_i.var_domain = bn['model'].states[node_i.var_name]
+node_i.var_domain_size = len(node_i.var_domain)  # maybe this is useless. I'll remove it when I'll be sure
+node_i.parents = ['HISTORY', 'HYPOVOLEMIA']
 parents_i_distinct_occurrences = []
 
-node_i_name = 'CO'
-node_i_possible_values = bn['model'].states[node_i_name]
+filter_node = samples[node_i.var_name] == 0
 
-filter_node = samples[node_i_name] == 0
+print("v_i =", node_i.var_domain, "=> r_i =", len(node_i.var_domain))
+print("pi_i =", node_i.parents)
 
-print("v_i =", node_i_possible_values, "=> r_i =", len(node_i_possible_values))
-print("pi_i =", parents_i_names)
-
-for i in samples.filter(items=parents_i_names).values.tolist():
+for i in samples.filter(items=node_i.parents).values.tolist():
     if i not in parents_i_distinct_occurrences:
         parents_i_distinct_occurrences.append(i)
 
 print("W_i =", parents_i_distinct_occurrences, "=> q_i =", len(parents_i_distinct_occurrences))
 
-print("selecting the columns", parents_i_names, ", ['"+node_i_name+"']")
-for i in samples.filter(items=parents_i_names+[node_i_name]).values.tolist():
+print("selecting the columns", node_i.parents, ", ['" + node_i.var_name + "']")
+for i in samples.filter(items=node_i.parents + [node_i.var_name]).values.tolist():
     print(i)
 
 for j in parents_i_distinct_occurrences:
     print("for j =", j)
     N_ij = 0
-    for k in node_i_possible_values:
-        print("\tcalculating N_ij set where '"+node_i_name+"' =", k, "( more precisely N_ijk where k =", k, ")")
-        pi_i_instances = samples.filter(items=parents_i_names) \
-            .where(samples[node_i_name] == k).copy().dropna().values.tolist()
+    for k in node_i.var_domain:
+        print("\tcalculating N_ij set where '" + node_i.var_name + "' =", k, "( more precisely N_ijk where k =", k, ")")
+        pi_i_instances = samples.filter(items=node_i.parents) \
+            .where(samples[node_i.var_name] == k).copy().dropna().values.tolist()
 
         # re-cast to int
         for i in pi_i_instances:
