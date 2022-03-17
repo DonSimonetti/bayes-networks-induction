f0b8a28054eed5ece8fbfe289aad08f5ef9591b4
matteodev <matt.simonetti@hotmail.com>
2022-03-17 17:34:37 +0100

    Deleted get_data_in_column_test.py

--- src/K2.py
+++ src/K2.py
@@ -1,39 +1,7 @@
 import math
 
 from src.node import Node
-
-
-def get_data_in_column(dataset, column):
-    print("Selecting", column)
-    result_list = []
-
-    column_index = dataset[0].index(column)
-
-    for i in range(1, len(dataset)):
-        result_list.append(dataset[i][column_index])
-
-    return result_list
-
-
-def DELETED_get_distinct_data_in_columns(dataset, columns):  #TODO DELETE
-    print("Selecting", columns)
-    result_list = []
-
-    columns_indexes = []
-    for column in columns:
-        columns_indexes.append(dataset[0].index(column))
-
-    # print(columns_indexes)
-
-    for i in range(1, len(dataset)):
-        vec = []
-        for index in columns_indexes:
-            vec.append(dataset[i][index])
-        if vec not in result_list:
-            result_list.append(vec)
-
-    return result_list
-
+from src.myFactorial import factorial
 
 #  'node_i'     is a Node from node.py
 #  'cases_df'   is a pandas DataFrame
@@ -54,11 +22,17 @@ def g_function(node_i, parents, cases_df):  # TODO
     for i in cases_df.filter(items=parents + [node_i.var_name]).values.tolist():
         print(i)
 
+    N_ij = [0 for j in range(len(parents_i_distinct_occurrences))]
+
+    # PT_j is the vector of the last production term for each j
+    PT_j = []
+
     for j in parents_i_distinct_occurrences:
         print("for j =", j)
         N_ijk = []
         for k in node_i.var_domain:
-            print("\tcalculating N_ij set where '" + node_i.var_name + "' =", k, "( more precisely N_ijk where k =", k,")")
+            print("\tcalculating N_ij set where '" + node_i.var_name + "' =", k, "( more precisely N_ijk where k =", k,
+                  ")")
             pi_i_instances = cases_df.filter(items=parents) \
                 .where(cases_df[node_i.var_name] == k).copy().dropna().values.tolist()
 
@@ -71,18 +45,19 @@ def g_function(node_i, parents, cases_df):  # TODO
             print("\t", pi_i_instances)
             N_ijk.append(pi_i_instances.count(j))
 
-        # now we have N_ijk for each possible k value of node_i
-        # now calculate their factorials, and multiply them
-        print("N_ijk =", N_ijk, "=> N_ij =", sum(N_ijk))
-        print("N_ijk! =", [math.factorial(i) for i in N_ijk], "=>", math.prod([math.factorial(i) for i in N_ijk]))
+        # now that we have N_ijk for each possible k value of node_i,
+        # we calculate their factorials, and multiply them
+        N_ij[parents_i_distinct_occurrences.index(j)] = sum(N_ijk)
+        # print("N_ijk =", N_ijk, "=> N_ij becomes:", N_ij)
+        PT_j.append(math.prod([math.factorial(i) for i in N_ijk]))
+        print("N_ijk! =", [math.factorial(i) for i in N_ijk], "=>", PT_j[parents_i_distinct_occurrences.index(j)])
 
     # here all the calculations
-    result = 0.0
-    # for j in range(parents_instances_size):  # the q_i loop
-    #     # TODO calculate result = (r_i - 1)!/(Nij + r_i - 1)!
-    #     # for k in range(r_i):
-    #     # TODO calculate result *= Nijk
-    #     print(j)
+    result = 1
+    for j in range(len(parents_i_distinct_occurrences)):  # the q_i loop
+        tmp = factorial(len(node_i.var_domain) - 1) / factorial(N_ij[j] + len(node_i.var_domain) - 1)
+        tmp *= PT_j[j]
+        result *= tmp
 
     return result
 
