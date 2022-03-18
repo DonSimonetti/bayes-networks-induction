cc5df4d6f1d34d70442867e3933095a02280bb21
DonSimonetti <matt.simonetti@hotmail.com>
2022-03-18 09:40:06 +0100

    Node.parents now is a set

--- src/node.py
+++ src/node.py
@@ -2,7 +2,7 @@ class Node:
 
     def __init__(self):
         self.var_name = ''
-        self.parents = []
+        self.parents = {}
         self.var_domain = []
         self.var_domain_size = 0  # I.E. the r_i possible value assignments
         return
