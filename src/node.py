775910795b7702d85820e23d5146705dbc19ab37
DonSimonetti <matt.simonetti@hotmail.com>
2022-03-18 17:51:10 +0100

    New constructor

--- src/node.py
+++ src/node.py
@@ -1,8 +1,8 @@
 class Node:
 
-    def __init__(self):
-        self.var_name = ''
+    def __init__(self, name, possible_values):
+        self.var_name = name
         self.parents = set()
-        self.var_domain = []
-        self.var_domain_size = 0  # I.E. the r_i possible value assignments
+        self.var_domain = possible_values
+        self.var_domain_size = len(self.var_domain)  # I.E. the r_i possible value assignments
         return
