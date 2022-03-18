3d812c54943db6b4aaa323a256e5ed7b77be8b63
DonSimonetti <matt.simonetti@hotmail.com>
2022-03-18 10:17:09 +0100

    Fixed errors

--- src/tests/g_function_test.py
+++ src/tests/g_function_test.py
@@ -8,8 +8,8 @@ samples = bnlearn.sampling(bn, 10)
 node_i = Node()
 node_i.var_name = 'CO'
 node_i.var_domain = bn['model'].states[node_i.var_name]
-parents = ['HISTORY', 'HYPOVOLEMIA']
+parents = {'HISTORY', 'HYPOVOLEMIA'}
 
 result = g_function(node_i, parents, samples)
 
-print("g_function result:",result)
+print("g_function result:", result)
