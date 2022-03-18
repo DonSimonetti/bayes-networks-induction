5dd9395ef09888b8d23a297975bd9a8c2216e9c8
DonSimonetti <matt.simonetti@hotmail.com>
2022-03-18 17:51:20 +0100

    Started working on it

--- src/bayes_induction.py
+++ src/bayes_induction.py
@@ -1,6 +1,7 @@
 import sys
 
 import pandas
+from node import Node
 from K2 import k2_procedure
 
 if len(sys.argv) != 2:
@@ -10,6 +11,24 @@ if len(sys.argv) != 2:
 dataset_df = pandas.read_csv(sys.argv[1])
 
 # TODO read the dataframe and instantiate every node
+nodes_array = []
+
+for i in dataset_df.columns:
+    # node = Node(i)
+
+    tmp = dataset_df['ANAPHYLAXIS']
+    print(tmp)
+
+    # nodes_states_dict = bn['model'].states.copy()
+    #
+    # for i in nodes_states_dict:
+    #     print(nodes_states_dict[i])
+
+print(len(nodes_array))
+
+
+
+
 # TODO put them in the 'nodes_array'
 # TODO provide an 'order_array'
 # TODO find appropriate 'max_parents'
