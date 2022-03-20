2c9190b9b95902481403513f629787d7a1447296
matteodev <matt.simonetti@hotmail.com>
2022-03-20 11:47:16 +0100

    Fixed error

--- src/tests/g_function_test.py
+++ src/tests/g_function_test.py
@@ -5,9 +5,7 @@ from src.node import Node
 bn = bnlearn.import_DAG('../../alarm.bif')
 samples = bnlearn.sampling(bn, 10)
 
-node_i = Node()
-node_i.var_name = 'CO'
-node_i.var_domain = bn['model'].states[node_i.var_name]
+node_i = Node('CO', bn['model'].states['CO'])
 parents = {'HISTORY', 'HYPOVOLEMIA'}
 
 result = g_function(node_i, parents, samples)
