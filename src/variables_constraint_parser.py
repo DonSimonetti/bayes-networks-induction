373fef3e2691f091164e9a52b998c8aa94738b22
matteodev <matt.simonetti@hotmail.com>
2022-03-21 09:04:20 +0100

    Finished variables_constraint_parser.py

--- src/variables_constraint_parser.py
+++ src/variables_constraint_parser.py
@@ -1,4 +1,5 @@
 import re
+import pickle
 
 source_file = open("alarm_variables_constraints.txt", "r")
 data = source_file.read()
@@ -11,5 +12,19 @@ lines_list = filtered_data.split("\n")
 
 for i in lines_list:
     lines_list[lines_list.index(i)] = re.sub(r"[=][\[][0-5][\]]", "", i).replace("}", "").replace("{", "=")
+lines_list.remove("")
 
-print(lines_list)
+variables = []
+for i in lines_list:
+    _str = i.split("=")
+    _str[1] = _str[1].split(",")
+    variables.append(_str)
+
+output_file = open("variables_constraints.obj","wb")
+pickle.dump(variables,output_file)
+output_file.close()
+
+input_file = open("variables_constraints.obj","rb")
+data = pickle.load(input_file)
+
+assert (data == variables)
