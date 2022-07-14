8db16a06642a31ccf08b7f4c98f58ae2eec9d2b7
DonSimonetti <matt.simonetti@hotmail.com>
2022-07-14 11:46:44 +0200

    Removed Node.var_domain_size attribute

--- src/node.py
+++ src/node.py
@@ -4,5 +4,4 @@ class Node:
         self.var_name = name
         self.parents = set()
         self.var_domain = possible_values
-        self.var_domain_size = len(self.var_domain)  # I.E. the r_i possible value assignments
         return
