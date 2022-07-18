5d95fe4d95d9f58b1b7a29a5095885fea0bf01f8
DonSimonetti <matt.simonetti@hotmail.com>
2022-07-18 10:28:37 +0200

    Fixing the case of empty parents set

--- src/gFunction.py
+++ src/gFunction.py
@@ -21,10 +21,11 @@ def g_function(node_i: Node, parents: set, cases_df: pandas.DataFrame) -> float:
 
     # calculate w_ij and q_i
     w_i = []
-    parents_cases = cases_df.filter(items=parents).values.tolist()
-    for row in parents_cases:
-        if row not in w_i:
-            w_i.append(row)
+    if len(parents) != 0:
+        parents_cases = cases_df.filter(items=parents).values.tolist()
+        for row in parents_cases:
+            if row not in w_i:
+                w_i.append(row)
     q_i = len(w_i)
 
     # calculate N_i matrix
@@ -32,17 +33,24 @@ def g_function(node_i: Node, parents: set, cases_df: pandas.DataFrame) -> float:
     columns.extend(parents)
     pippo = cases_df.filter(items=columns)
 
-    N_i = [[0 for j in w_i] for k in node_i.var_domain]
-    for k in node_i.var_domain:
-        for w_ij in w_i:
+    if q_i != 0:
+        N_i = [[0 for j in w_i] for k in node_i.var_domain]
+        for k in node_i.var_domain:
             pluto = pippo.where(pippo[node_i.var_name] == k)
-            pluto.where(pippo[parents] == w_ij, inplace=True)
-            pluto.dropna(inplace=True)
-            N_i[k][w_i.index(w_ij)] = len(pluto.index)
+            for w_ij in w_i:
+                if q_i != 0:
+                    pluto.where(pippo[parents] == w_ij, inplace=True)
+                pluto.dropna(inplace=True)
+                N_i[k][w_i.index(w_ij)] = len(pluto.index)
+    else:
+        N_i = []
+        for k in node_i.var_domain:
+            N_i.append([len(pippo.where(pippo[node_i.var_name] == k).dropna())])
 
     # calculate g_function value
     g_value = 1
-    for j in range(q_i):
+    j = 0
+    while j == 0 or j < q_i:
         N_ij = 0
         for i in range(r_i):
             N_ij += N_i[i][j]
@@ -51,7 +59,8 @@ def g_function(node_i: Node, parents: set, cases_df: pandas.DataFrame) -> float:
         for i in range(r_i):
             factorials_prod *= math.factorial(N_i[i][j])
 
-        tmp1 = math.factorial(r_i-1)
+        tmp1 = math.factorial(r_i - 1)
         tmp2 = math.factorial(N_ij + r_i - 1)
-        g_value *= tmp1*factorials_prod/tmp2
+        g_value *= tmp1 * factorials_prod / tmp2
+        j += 1
     return g_value
--- src/tests/g_function_test.py
+++ src/tests/g_function_test.py
@@ -6,7 +6,8 @@ bn = bnlearn.import_DAG('../../alarm.bif')
 samples = bnlearn.sampling(bn, 10)
 
 node_i = Node('CO', bn['model'].states['CO'])
-parents = {'HISTORY', 'HYPOVOLEMIA'}
+# parents = {'HISTORY', 'HYPOVOLEMIA'}
+parents = {}
 
 result = g_function(node_i, parents, samples)
 
