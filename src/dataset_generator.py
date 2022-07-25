1fa3d04a36c1171c43b3bcddff65f315bf70fd26
matteo <matt.simonetti@hotmail.com>
2022-07-25 12:48:57 +0200

    Using remote alarm.bif

--- src/dataset_generator.py
+++ src/dataset_generator.py
@@ -13,7 +13,7 @@ if not sys.argv[1].isnumeric():
 samples_count = int(sys.argv[1])
 print("Generating", samples_count, "samples with bnlearn")
 
-bn = bnlearn.import_DAG('../alarm.bif')
+bn = bnlearn.import_DAG('alarm')
 samples = bnlearn.sampling(bn, samples_count)
 filename = samples_count.__str__()+"_samples_dataset"
 samples.to_csv(filename+".csv", index=False)
