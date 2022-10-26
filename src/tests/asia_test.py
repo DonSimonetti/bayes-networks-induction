c8d1a4599fed2e5e2aec711bcbbb3e69f981dd22
matteo <matt.simonetti@hotmail.com>
2022-10-26 16:48:33 +0200

    Stuff

--- src/tests/asia_test.py
+++ src/tests/asia_test.py
@@ -2,18 +2,17 @@ import bnlearn as bn
 from pgmpy.models import BayesianNetwork
 import graphviz
 # Load asia DAG
-model_true = bn.import_DAG('asia')
+model_true = bn.import_DAG('alarm')
 # plot ground truth
-graph = model_true["model"]
-graph.plot()
+G = bn.plot(model_true)
 
 # Sampling
 df = bn.sampling(model_true, n=10000)
 # Structure learning of sampled dataset
-model_learned = bn.structure_learning.fit(df, methodtype='hc', scoretype='bic')
+model_learned = bn.structure_learning.fit(df, methodtype='hc', scoretype='k2')
 
 # Plot based on structure learning of sampled data
-#bn.plot(model_learned, pos=G['pos'], interactive=True)
+bn.plot(model_learned)
 # Compare networks and make plot
-#bn.compare_networks(model_true, model_learned, pos=G['pos'])
+bn.compare_networks(model_true, model_learned, pos=G['pos'])
 
