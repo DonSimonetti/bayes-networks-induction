3ae0df03b1b4130be501b74b64334abdd92af767
DonSimonetti <matt.simonetti@hotmail.com>
2022-11-07 12:32:41 +0100

    Added script to execute all datasets

---
+++ src/run_all_datasets.sh
@@ -0,0 +1,8 @@
+python bayes_induction.py 100_samples_dataset.csv 
+python bayes_induction.py 200_samples_dataset.csv 
+python bayes_induction.py 500_samples_dataset.csv 
+python bayes_induction.py 1000_samples_dataset.csv 
+python bayes_induction.py 3000_samples_dataset.csv 
+python bayes_induction.py 10000_samples_dataset.csv 
+python bayes_induction.py 20000_samples_dataset.csv
+
