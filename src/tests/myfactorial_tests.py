4a6402b9d5f7742d36dff0a5af6d2ad3a59f650e
DonSimonetti <matt.simonetti@hotmail.com>
2022-11-15 09:11:26 +0100

    Deleted old stuff

--- src/tests/myfactorial_tests.py
+++
@@ -1,8 +0,0 @@
-from src.myFactorial import factorial, generate_factorials, cached_values
-
-# Test
-generate_factorials(11)
-
-print(cached_values)
-print("min calculated value:", len(cached_values) - 1)
-print(factorial(40))
