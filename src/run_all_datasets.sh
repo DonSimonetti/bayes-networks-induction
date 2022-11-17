8e32cb66137291a5d0f04fbac1ffadc096bb45fd
DonSimonetti <matt.simonetti@hotmail.it>
2022-11-17 11:15:18 +0000

    Added test for 2000 cases

--- src/run_all_datasets.sh
+++ src/run_all_datasets.sh
@@ -2,6 +2,7 @@ python dataset_generator.py 100
 python dataset_generator.py 200
 python dataset_generator.py 500
 python dataset_generator.py 1000
+python dataset_generator.py 2000
 python dataset_generator.py 3000
 python dataset_generator.py 10000
 python dataset_generator.py 20000
@@ -10,7 +11,7 @@ python bayes_induction.py 100_samples_dataset.csv
 python bayes_induction.py 200_samples_dataset.csv
 python bayes_induction.py 500_samples_dataset.csv
 python bayes_induction.py 1000_samples_dataset.csv
+python bayes_induction.py 2000_samples_dataset.csv
 python bayes_induction.py 3000_samples_dataset.csv
 python bayes_induction.py 10000_samples_dataset.csv
 python bayes_induction.py 20000_samples_dataset.csv
-
